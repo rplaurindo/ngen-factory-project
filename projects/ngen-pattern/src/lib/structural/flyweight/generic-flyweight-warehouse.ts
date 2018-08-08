59356075062a022c81fe480e5e35298b74c00ac3
Rafael Laurindo <rafaelplaurindo@gmail.com>
2018-08-08 18:09:30 -0300

    remove comments

--- projects/ngen-pattern/src/lib/structural/flyweight/generic-flyweight-warehouse.ts
+++ projects/ngen-pattern/src/lib/structural/flyweight/generic-flyweight-warehouse.ts
@@ -20,33 +20,3 @@ export class GenericFlyweightWarehouse<T> {
     }
 
 }
-
-// class TestModel implements GenericAbstractFlyweight<TestModel> {
-
-//     constructor(...args) {
-
-//     }
-
-//     getConcrete(): TestModel {
-//         return this;
-//     }
-
-// }
-
-// class TestService {
-
-//     constructor(private flyweightFactory: GenericFlyweightWarehouse<TestModel>) {
-//         flyweightFactory.add(TestModel, new TestModel());
-//     }
-
-// }
-
-// class TestComponent {
-
-//     private testModel: TestModel;
-
-//     constructor(private testService: TestService) {
-//         // testService
-//     }
-
-// }
